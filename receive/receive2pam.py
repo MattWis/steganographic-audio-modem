9f364def168b762938d50af4a7dde1502227baf5
MattWis <matthew.wismer@students.olin.edu>
2013-12-10 14:09:27 -0500

    Remove smooth function

--- receive/receive2pam.py
+++ receive/receive2pam.py
@@ -61,52 +61,6 @@ def maxIndex(list):
             return (idx, val)
     return (-1, maximum)
 
-
-def smooth(x,window_len=11):
-    """smooth the data using a window with requested size.
-    
-    This method is based on the convolution of a scaled window with the signal.
-    The signal is prepared by introducing reflected copies of the signal 
-    (with the window size) in both ends so that transient parts are minimized
-    in the begining and end part of the output signal.
-    
-    input:
-        x: the input signal 
-        window_len: the dimension of the smoothing window; should be an odd integer
-        window: the type of window from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'
-            flat window will produce a moving average smoothing.
- 
-    output:
-        the smoothed signal
-        
-    example:
- 
-    t=linspace(-2,2,0.1)
-    x=sin(t)+randn(len(t))*0.1
-    y=smooth(x)
-    
-    see also: 
-    
-    np.hanning, np.hamming, np.bartlett, np.blackman, np.convolve
-    scipy.signal.lfilter
- 
-    NOTE: length(output) != length(input), to correct this: return y[(window_len/2-1):-(window_len/2)] instead of just y.
-    """
-    if x.ndim != 1:
-        raise ValueError, "smooth only accepts 1 dimension arrays."
-    if x.size < window_len:
-        raise ValueError, "Input vector needs to be bigger than window size."
-    if window_len<3:
-        return x
- 
-    s=np.r_[x[window_len-1:0:-1],x,x[-1:-window_len:-1]]
-    
-    w = np.hanning(window_len)
-    # w = np.ones(window_len, 'd')
- 
-    y=np.convolve(w/w.sum(),s,mode='valid')
-    return y
-
 def get_next_data_from_wav(wav_file, num_seconds):
     raw_data = wav_file.readframes(44100 * num_seconds)
 
@@ -155,10 +109,5 @@ print randomData()
 
 # impulse = channel[maxIdx:]
 # plt.plot(correlated)
-# plt.show()
 
-# channel = np.fft.fft(impulse)
-# smooth_channel = smooth(channel, 500)
-# freq = np.fft.fftfreq(impulse.shape[-1])
-# plt.plot(smooth(freq * 44100, 500), smooth(np.sqrt(channel.real**2 + channel.imag**2), 500), 'k')
 plt.show()
